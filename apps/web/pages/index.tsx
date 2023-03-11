30460cdcc01d7db189ae77d1d2c72dd8352420f9
caiooaragao <97796450+caiooaragao@users.noreply.github.com>
2023-03-11 17:46:26 -0300

    Add files via upload

---
+++ apps/web/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Home } from "ui"
+
+const index = () => {
+  return (
+    <Home />
+  )
+}
+
+export default index
---
+++ packages/ui/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+export * from "./view/Home";
+export * from  "./atomos/botoes/BotaoAzul";
+export * from "./atomos/botoes/BotaoBranco/BotaoBranco";
+export * from "./atomos/LogoInicial/LogoInicial";
